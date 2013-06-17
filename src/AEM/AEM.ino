b7aded9e1e2e80162230453287eb1e1be7d77d63
Duncan Greer <duncangreer@gmail.com>
2013-06-17 22:50:30 +1000

    Added RPM sensor code

--- src/AEM/AEM.ino
+++ src/AEM/AEM.ino
@@ -8,8 +8,20 @@
 
 #define FUEL_LL_PIN  3
 
+
+/* Attach hall sensor to interrupt 5 - 
+   See http://arduino.cc/en/Main/ArduinoBoardMega2560 
+   and http://arduino.cc/en/Reference/AttachInterrupt 
+ */
+#define RPM_INT 5 
+#define RPM_PIN 18  
+
 int pin;
 int fuel_state;
+
+volatile unsigned long rpm_count;
+unsigned long last_time;
+unsigned short rpm;
 
 void setup()
 {
@@ -23,6 +35,10 @@ void setup()
   // fuel sensor
   pinMode(FUEL_LL_PIN,INPUT);
   
+  // RPM sensor
+  attachInterrupt(RPM_INT,rpm_tick,RISING);
+  rpm_count = 0;
+  last_time = millis();
 
   // initialize CAN bus class
   // this class initializes SPI communications with MCP2515
@@ -47,9 +63,23 @@ void loop()
   //digitalWrite(7,(fuel_state == 1) ? HIGH : LOW);
   digitalWrite(7,fuel_state);
   
+  // update RPM
+  if (rpm_count > 20)
+  {
+    
+     unsigned long time_now = millis();
+     unsigned long time_since = time_now - last_time;
+     
+     rpm = (60000 * rpm_count)/time_since ;
+    
+     last_time = time_now;
+     rpm_count = 0;   
+  }
   
   Serial.print("Fuel: ");
   Serial.print(fuel_state,DEC);
+  Serial.print(" RPM: ");
+  Serial.print(rpm, DEC);
   Serial.println();
   
   
@@ -64,5 +94,12 @@ void loop()
   delay(500);
   
 }
+
+void rpm_tick()
+{
+  ++rpm_count;
+}
+
+
 
 
